bcc26da02a884af6a590643cb4b4e83a4a701fad
--global <allmie@naver.com>
2025-07-03 08:10:22 +0900

    Schema definition(typeDefs, scalar type)

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+// schema definition language
 const typeDefs = gql`
-  type Query {
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID
     text: String
+    author: User
+  }
+  type Query {
+    allTweets: [Tweet]
   }
 `;
 
